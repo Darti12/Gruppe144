76fc6bbae49479ce4d2ed8c28c002aa82ed71062
filiphag <filiphag@stud.ntnu.no>
2018-03-15 14:27:01 +0100

    lainn to sql spørringer
    en eller annen feil når jeg kjører det her :/

--- DatabaseEksempel/src/sample/Controller.java
+++ DatabaseEksempel/src/sample/Controller.java
@@ -1,4 +1,116 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import java.lang.*;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class Controller implements Initializable{
+    public Connection myConn;
+    @FXML public TextField apparatNavn;
+    @FXML public TextField apparatFunksjon;
+    @FXML public TextField øvelseNavn;
+    @FXML public TextField treningsøktDato;
+    @FXML public TextField treningsøktPersonligForm;
+    @FXML public TextField treningsøktPrestasjon;
+    @FXML public TextField treningsøktVarighet;
+    @FXML public TextField treningsøktInformasjon;
+
+
+    public Controller() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            this.myConn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/filiphag_prosjekt", "filiphag_server", "123abc");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    private void registrerApparat(){
+        try {
+            Statement myStatement2 = myConn.createStatement();
+            ResultSet rs2 = myStatement2.executeQuery("SELECT MAX(ApparatID) FROM Apparat");
+            rs2.next();
+            int tallID = Integer.parseInt(rs2.getString("MAX(ApparatID)")) + 1;
+
+            String navn = apparatNavn.getText();
+            String funksjon = apparatFunksjon.getText();
+
+            Statement myStatement = myConn.createStatement();
+            String sql = "Insert into Apparat VALUES(" + tallID + ", '" + navn + "', '" + funksjon +"')";
+            System.out.println(sql);
+            myStatement.executeUpdate(sql);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    private void registrerØvelse(){
+        try {
+            Statement myStatement2 = myConn.createStatement();
+            ResultSet rs2 = myStatement2.executeQuery("SELECT MAX(ØvelseID) FROM Øvelse");
+            rs2.next();
+            int tallID = Integer.parseInt(rs2.getString("MAX(ØvelseID)")) + 1;
+
+            String navn = øvelseNavn.getText();
+
+            Statement myStatement = myConn.createStatement();
+            String sql = "Insert into Øvelse VALUES(" + tallID + ", '" + navn + "')";
+            System.out.println(sql);
+            myStatement.executeUpdate(sql);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    private void registrerTreningsøkt(){
+        try {
+            Statement myStatement2 = myConn.createStatement();
+            ResultSet rs2 = myStatement2.executeQuery("SELECT MAX(ØvelseID) FROM Øvelse");
+            rs2.next();
+            int tallID = Integer.parseInt(rs2.getString("MAX(ØvelseID)")) + 1;
+
+            String navn = "";
+
+            Statement myStatement = myConn.createStatement();
+            String sql = "Insert into Øvelse VALUES(" + tallID + ", '" + navn + "')";
+            System.out.println(sql);
+            myStatement.executeUpdate(sql);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("hei..");
+    }
 }
--- DatabaseEksempel/src/sample/Main.java
+++ DatabaseEksempel/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Database Prosjekt Øving");
+        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
 
