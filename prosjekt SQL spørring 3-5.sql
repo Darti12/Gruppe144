a9746856de315c5280e1fd2f8fdb2b9f47e61585
Katrinhh <katrine.hveding@hotmail.com>
2018-03-15 12:50:44 +0100

    Added more queries

--- prosjekt SQL spørring 3-5.sql
+++ prosjekt SQL spørring 3-5.sql
@@ -1,4 +1,22 @@
-#3 
+
+
+
+INSERT INTO Notat VALUES (1, "BLA BLA VLA", 1), (2, "BLALALAL", 2);
+
+INSERT INTO Øvelse VALUES (1, "Pushups"), (2, "Situps"), (3, "Roing"), (4, "Vekter");
+
+INSERT INTO ApparatØvelse VALUES (3), (4);
+
+INSERT INTO ØvelseMedApparat VALUES (3, 1, 30, 4), (4, 2, 20, 3);
+
+INSERT INTO Friøvelse VALUES (1, "Vanlige pushups"), (2, "Vanlige situps");
+
+INSERT INTO Øvelsesøkt VALUES (1, 1), (1, 3), (2, 4), (4, 2);
+
+INSERT INTO Øvelsesgruppe VALUES (1, "Styrke", "Styrkende øvelser"), (2, "Armstyrke", "Øvelser for armstyrke"),
+								 (3, "Core", "Øvelser for kjernemuskulatur");
+                                 
+INSERT INTO ØvelseIGruppe VALUES (1, 2), (1, 1), (2, 3), (2, 1), (3, 1), (3, 2), (3, 3), (4, 1);#3 
 SELECT T.TøID, T.Dato, T.Tid, T.PersonligForm, T.Prestasjon, T.Varighet, T.Informasjon
 FROM Treningsøkt AS T 
 ORDER BY T.Dato AND T.Tid;
